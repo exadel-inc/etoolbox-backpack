dc813b4e54ac8c72982cf9f8dbee46d90346228b
abestsenny <1305Bes1305>
2020-01-10 13:52:57 +0300

    Download btn

--- ui.apps/src/main/content/jcr_root/apps/backpack/admin/console/components/build/clientlibs/js/build.js
+++ ui.apps/src/main/content/jcr_root/apps/backpack/admin/console/components/build/clientlibs/js/build.js
@@ -97,7 +97,10 @@ $(function () {
                         });
                         $buildLog.append('<h4>Approximate referenced resources size: ' + bytesToSize(data.dataSize) + '</h4>');
                     }
-                    $downloadBtn.show();
+                    if (!testBuild) {
+                        $downloadBtn.show();
+                    }
+
                 } else {
                     setTimeout(updateLog, 1000);
                     $buildButton.text('Rebuild');
