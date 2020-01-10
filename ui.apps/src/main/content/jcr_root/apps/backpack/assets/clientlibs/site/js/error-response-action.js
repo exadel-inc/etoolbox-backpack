e5d93412e19b36c58d194f4e4f00a4c1400c0cd0
abestsenny <1305Bes1305>
2020-01-10 13:49:57 +0300

    error response

---
+++ ui.apps/src/main/content/jcr_root/apps/backpack/assets/clientlibs/site/js/error-response-action.js
@@ -0,0 +1,12 @@
+(function(window) {
+    $(window).adaptTo("foundation-registry").register("foundation.form.response.ui.error", {
+        name: "errorResponseCreated",
+        handler: function(form, data, xhr, error, errorThrown) {
+            var title = Granite.I18n.get("Error");
+            var message = xhr.responseJSON.buildLog;
+
+            var ui = $(window).adaptTo("foundation-ui");
+            ui.alert(title, message, "error");
+        }
+    });
+})(window);
