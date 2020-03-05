fa611aebf7d04f7ae66ba711e5ec34971b644a55
acharniakou <alexey.chernyakov00@gmail.com>
2020-03-05 10:14:36 +0300

    [EDMTP-219] Fixed bug with displaying wrong info at referenced resources

--- core/src/main/java/com/exadel/aem/backpack/core/services/impl/PackageServiceImpl.java
+++ core/src/main/java/com/exadel/aem/backpack/core/services/impl/PackageServiceImpl.java
@@ -324,6 +324,9 @@ public class PackageServiceImpl implements PackageService {
                                      final PackageInfo packageBuildInfo,
                                      final DefaultWorkspaceFilter filter) {
         JcrPackage jcrPackage = null;
+        if (packageBuildInfo.getPackagePath() != null) {
+            packageInfos.invalidate(packageBuildInfo.getPackagePath());
+        }
         try {
             JcrPackageManager packMgr = PackagingService.getPackageManager(userSession);
             if (!filter.getFilterSets().isEmpty()) {
