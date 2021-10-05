cb2b93f1feea40772f52b8b4541dfeb0c8be7b1c
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-10-05 18:57:43 +0530

    Changed build message in the Log Section from 'Approximate size of the assets in the package: 0 Bytes' to 'There are no assets in the package'

--- ui.apps/src/main/content/jcr_root/apps/etoolbox-backpack/admin/console/components/build/clientlibs/js/build.js
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-backpack/admin/console/components/build/clientlibs/js/build.js
@@ -256,7 +256,7 @@ $(function () {
                         $.each(data.log, function (index, value) {
                             $buildLog.append('<div>' + value + '</div>');
                         });
-                        $buildLog.append('<h4>Approximate size of the assets in the package: ' + bytesToSize(data.dataSize) + '</h4>');
+                        $buildLog.append(<h4>There are no assets in the package</h4>);
                         scrollLog();
                     }
                 } else {
