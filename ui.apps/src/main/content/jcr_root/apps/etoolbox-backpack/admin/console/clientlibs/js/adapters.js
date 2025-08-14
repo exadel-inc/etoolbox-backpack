b9cb0149def17135cb777041bc20d8cb1f7856b4
alesun <alesun@exadel.com>
2025-08-14 16:01:27 +0200

    [TECH] add common form response error handler

--- ui.apps/src/main/content/jcr_root/apps/etoolbox-backpack/admin/console/clientlibs/js/adapters.js
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-backpack/admin/console/clientlibs/js/adapters.js
@@ -5,7 +5,7 @@
     const foundationRegistryAPI = $(window).adaptTo('foundation-registry');
 
     foundationRegistryAPI.register('foundation.form.response.ui.success', {
-        name: 'foundation.prompt.open',
+        name: 'backpack.form.prompt.open',
         handler: function (form, config, data) {
             if (!data) return;
             const isWarning = data.status === 'WARNING';
@@ -19,27 +19,7 @@
             } else {
                 window.location.reload();
             }
-        }
-    });
-
-    foundationRegistryAPI.register('foundation.form.response.ui.error', {
-        name: 'errorResponseCreated',
-        handler: function (form, data, xhr) {
-            let message = '';
-            if (xhr.responseJSON) {
-                message = xhr.responseJSON.log;
-            } else if (xhr.responseText) {
-                try {
-                    const response = JSON.parse(xhr.responseText);
-                    if (response && response.log) {
-                        message = response.log;
-                    }
-                } catch (e) {
-                    console.error('Failed to parse responseText:', e);
-                }
-            }
-
-            $(window).adaptTo('foundation-ui').alert('Error', message, 'error');
+            return false;
         }
     });
 
